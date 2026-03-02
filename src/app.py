4ccfe9135f09dbbbb0ee06f20dc4dc10f88c585b
Rammo <shuamglebshi@gmail.com>
2026-03-02 22:38:12 +0400

    add .flake8

--- src/app.py
+++ src/app.py
@@ -6,23 +6,25 @@ app = Flask(__name__)
 
 ENV = os.getenv("ENV", "DEV")
 
-@app.route('/')
-def home():
-    return jsonify({
-        "status": "online",
-        "message": "Flask App running on AWS EC2",
-        "provisioned_by": "Terraform",
-        "configured_by": "Ansible"
-    })
 
-@app.route('/health')
+@app.route("/")
+def home():
+    return jsonify(
+        {
+            "status": "online",
+            "message": "Flask App running on AWS EC2",
+            "provisioned_by": "Terraform",
+            "configured_by": "Ansible",
+        }
+    )
+
+
+@app.route("/health")
 def health():
     db_message = check_db_status()
     status_code = 200 if "Connected" in db_message else 500
-    return jsonify({
-        "service": "active",
-        "database": db_message
-    }), status_code
+    return jsonify({"service": "active", "database": db_message}), status_code
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
--- src/database.py
+++ src/database.py
@@ -2,26 +2,28 @@ import os
 import psycopg2
 from psycopg2 import OperationalError
 
+
 def get_db_connection():
     try:
         conn = psycopg2.connect(
-            host=os.getenv('DB_HOST', 'db'),
-            database=os.getenv('DB_NAME', 'flask_db'),
-            user=os.getenv('DB_USER', 'postgres'),
-            password=os.getenv('DB_PASS', 'password'),
-            connect_timeout=5
+            host=os.getenv("DB_HOST", "db"),
+            database=os.getenv("DB_NAME", "flask_db"),
+            user=os.getenv("DB_USER", "postgres"),
+            password=os.getenv("DB_PASS", "password"),
+            connect_timeout=5,
         )
         return conn
     except OperationalError as e:
         print(f"Error connecting to RDS: {e}")
         return None
 
+
 def check_db_status():
     conn = get_db_connection()
     if conn:
         try:
             cur = conn.cursor()
-            cur.execute('SELECT version();')
+            cur.execute("SELECT version();")
             db_version = cur.fetchone()
             cur.close()
             conn.close()
